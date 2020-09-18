97c79cc6a4bffb1c68145bb11acd9f65a63713b1
Nhat Dinh <nguyendinhnhat206@gmail.com>
2020-09-18 00:02:00 -0400

    second commit

--- src/main/java/nhatdinh/bikespecialties/BikeSpecialtiesApplication.java
+++ src/main/java/nhatdinh/bikespecialties/BikeSpecialtiesApplication.java
@@ -1,24 +1,29 @@
 package nhatdinh.bikespecialties;
 
 import nhatdinh.bikespecialties.db.repo.BrandRepository;
+import nhatdinh.bikespecialties.db.repo.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BikeSpecialtiesApplication {
+public class BikeSpecialtiesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BikeSpecialtiesApplication.class, args);
 	}
 
-//	@Autowired
-//	BrandRepository brandRepository;
-//
-//	@Override
-//	public void run(String... args) throws Exception {
-//		System.out.println("\n*************Original Brand*************");
-//		brandRepository.findAll().forEach(System.out::println);
-//	}
+	@Autowired
+	BrandRepository brandRepository;
+
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("=========Search by last name=============");
+		String lastName = "en";
+		customerRepository.searchForLastNameLike(lastName).forEach(System.out::println);
+	}
 }
